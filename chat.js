150b2f8525cbb131cf0323937c07f73a22bad61b
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2023-03-07 19:32:05 +0900

    Big change too

--- chat.js
+++ chat.js
@@ -1,57 +1,135 @@
-import got from 'got';
+// get the form and input elements
+const submitbtn = document.getElementById('submitbtn');
+const input = document.getElementById('question-input');
 
-const f = document.querySelector("form");
-
-f.addEventListener("submit", async (e) => {
+// handle the form submission
+submitbtn.addEventListener("submit", (e) => {
+  // prevent the default form submission behavior
   e.preventDefault();
 
-  const questionInput = document.getElementById('question-input');
-  const question = questionInput.value;
-  const endpoint = `https://chat-taro350.vercel.app/chat?question=${encodeURIComponent(question)}`;
-  console.log(endpoint);
-
-  const responseTextarea = document.getElementById('response-textarea');
-  responseTextarea.value = 'Loading...'; // Display a loading message while waiting for the response
-
-  const jsonresponse = await got.post(endpoint);
-  console.log(jsonresponse)
-
-  // fetch(endpoint, {
-  //       method : "GET",
-        
-  //   })
-  //   .then(response => {
-  //     console.log(response)
-  //     response.json()
-  //   })
-  //   .then(data => {
-  //     console.log(data);
-  //     responseTextarea.value = JSON.stringify(data, null, 2); // Display the response data in the textarea
-  //   })
-  //   .catch(error => {
-  //     console.error('Error caught:', error);
-  //     responseTextarea.value = 'An error occurred'; // Display an error message if there was an error
-  //   });
+  alert("hey")
+  
+  // create a new URLSearchParams object
+  const searchParam = new URLSearchParams();
+  
+  // add the form data to the URLSearchParams object
+  searchParam.append('question', input.value);
+  
+  // send the form data to the server using fetch API
+  fetch('https://localhost:3000/chat?' + searchParam.toString())
+  .then(response => {
+    resultTextarea.value = response.choises[0].text()
+  })
+  .catch(error => {
+    console.log('Error: ', error);
+  });
 });
 
-// function submitQuestion() {
+
+// const myForm = document.getElementById('myForm');
+// const resultTextarea = document.getElementById('response-textarea');
+
+// myForm.addEventListener('submit', (event) => {
+// event.preventDefault();
+
+// const formData = new FormData(myForm);
+
+// const questionInput = document.getElementById('question-input');
+// const inputQuery = questionInput.name;
+// const inputData = questionInput.value;
+
+// responseTextarea.value = 'Loading...';
+
+// var delayInMilliseconds = 7000; //7 second
+
+// setTimeout(function() {
+// 	fetch(`/chat?${inputQuery}=${inputData}`, {
+// 		method: 'GET',
+// 		headers: {
+// 	      "Content-Type": "application/json",
+// 	    },
+// 	})
+// 	.then(response => response.choises[0].text())
+// 	.then(data => {
+// 		resultTextarea.value = data;
+// 	})
+// 	.catch(error => {
+// 		console.error(error);
+// 	});
+
+// }, delayInMilliseconds);
+
+
+
+// import got from 'got';
+
+// const f = document.querySelector("form");
+
+// f.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const form = e.target; // find closest form element
+//   const formData = new FormData(form); // create new FormData object from form data
+//   const url = new URL(form.action, window.location.origin); // create new URL object from form action and current domain
+
 //   const questionInput = document.getElementById('question-input');
-//   const question = questionInput.value;
-//   const endpoint = `http://localhost:8080/?question=${encodeURIComponent(question)}`;
-//   console.log(endpoint);
+//   const inputQuery = questionInput.name;
+//   const inputData = questionInput.value;
+
+//   // const inputQuery = form.previousElementSibling.name; // get input query/name from previous sibling element of button
+//   // const inputData = form.previousElementSibling.value; // get input data from previous sibling element of button
+//   formData.append(inputQuery, inputData); // append input query and data to FormData object
+//   const fullUrl = url.origin + url.pathname + '?' + inputQuery + '=' + inputData; // create full URL path
+//   console.log(`Full URL: ${fullUrl}`);
+
+  
+
 //   const responseTextarea = document.getElementById('response-textarea');
 //   responseTextarea.value = 'Loading...'; // Display a loading message while waiting for the response
-//   fetch(endpoint, {
-//         method : "GET",
-//         mode: 'no-cors'
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//     	console.log(data);
-// 		responseTextarea.value = JSON.stringify(data, null, 2); // Display the response data in the textarea
-//     })
-//     .catch(error => {
-// 		console.error('Error:', error);
-// 		responseTextarea.value = 'An error occurred'; // Display an error message if there was an error
-//     });
-// }
+
+//   const endpoint = `https://localhost:3000/chat?question=${encodeURIComponent(question)}`;
+//   console.log(endpoint);
+
+//   const jsonresponse = got.get(endpoint);
+//   console.log(jsonresponse)
+//   responseTextarea.value = JSON.stringify(jsonresponse, null, 2);
+
+//   // fetch(endpoint, {
+//   //       method : "GET",
+        
+//   //   })
+//   //   .then(response => {
+//   //     console.log(response)
+//   //     response.json()
+//   //   })
+//   //   .then(data => {
+//   //     console.log(data);
+//   //     responseTextarea.value = JSON.stringify(data, null, 2); // Display the response data in the textarea
+//   //   })
+//   //   .catch(error => {
+//   //     console.error('Error caught:', error);
+//   //     responseTextarea.value = 'An error occurred'; // Display an error message if there was an error
+//   //   });
+// });
+
+// // function submitQuestion() {
+// //   const questionInput = document.getElementById('question-input');
+// //   const question = questionInput.value;
+// //   const endpoint = `http://localhost:8080/?question=${encodeURIComponent(question)}`;
+// //   console.log(endpoint);
+// //   const responseTextarea = document.getElementById('response-textarea');
+// //   responseTextarea.value = 'Loading...'; // Display a loading message while waiting for the response
+// //   fetch(endpoint, {
+// //         method : "GET",
+// //         mode: 'no-cors'
+// //     })
+// //     .then(response => response.json())
+// //     .then(data => {
+// //     	console.log(data);
+// // 		responseTextarea.value = JSON.stringify(data, null, 2); // Display the response data in the textarea
+// //     })
+// //     .catch(error => {
+// // 		console.error('Error:', error);
+// // 		responseTextarea.value = 'An error occurred'; // Display an error message if there was an error
+// //     });
+// // }
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import got from 'got';
+import url from 'url';
 import path from 'path'
+import https from 'https';
+import http from 'http';
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+var querystring = require('querystring');
+
+import { Buffer } from 'node:buffer';
+
+
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
 
 const app = express();
 const port = 3000;
@@ -10,64 +22,151 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.json());
 
 // enabling CORS
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Cache-Control, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Cache-Control, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Content-Length, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
 
+// home
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname });
   console.log(`Root directly done`);
-  console.log(__dirname)
+  console.log(`Domain : ${__dirname}`)
+  console.log(path.join(__dirname, 'logo.png'))
 });
 
+// chat
 app.get('/chat', async function (req, res) {
-  console.log(`Params for query : ${req.query.question}`);
+  let queryParam = url.parse(req.url, true).query.question
+
+  console.log(`----- Route : ${req.url}`);
+  console.log(`----- Params for path : ${req.path}`);
+  console.log(`----- Params for query : ${queryParam}`);
 
   // only ?question= in URL
-  if (req.query.question != undefined) {
+  if (queryParam !== undefined) {
+      console.log('queryParam is defined');
 
-      const userInputText = req.query.question;
-      const url = 'https://api.openai.com/v1/engines/davinci/completions';
-      const requestData = {
-        "prompt": `Say this is a ${userInputText}`,
-        "max_tokens": 7,
-        "temperature": 0.5,
+      const jsonpayload = JSON.stringify({
+        "prompt" : "Say this is a hi",
+        "max_tokens" : 7,
+        "temperature" : 0.5,
         "frequency_penalty": 0.5
-      };
-      const headers = {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      });
+
+      // const agent = new http.Agent({
+      //     keepAlive: true,
+      //     maxSockets: 1
+      // });
+
+      console.log(jsonpayload)
+
+
+      const options = {
+        hostname: "api.openai.com",
+        port: 443,
+        path: '/v1/engines/davinci/completions',
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+          'Content-Length' : jsonpayload.length,
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        }
       };
 
-      try {
-        const jsonresponse = await got.post(url, { json: requestData, headers: headers }).json();
-        output = `${prompt}${jsonresponse.choices[0].text}`;
-        console.log(`Success! Here's the output :${output}`);
-        return output
-      } catch (err) {
-        console.log(`Agh, erroe caught : ${err}`);
-      }
-    
+      let rawData = [];
 
-    
+      console.log("Let's reqeust! to OpenAI");
+
+      // Actually express skip this part and get to the req.write(jsonpayload)
+      // and that's why we can send HTTP POST request with JSON payload!!
+      // WTF!! 
+      // This is actually happening behind the scenes!
+      // *************************************************************
+      // const req = http.request(options);
+      // req.end();
+      // req.on('connect', (res, socket, head) => {
+      //     console.log('got connected!');
+      //     ..
+      // });
+      // https://nodejs.org/api/http.html#event-connect
+      // *************************************************************
+
+      const req = https.request(options, (res) => {
+        console.log('----- StatusCode for OpenAI req:', res.statusCode);
+        console.log('----- Headers of response :', res.headers);
+        res.setEncoding('utf8');
+
+        res.on('data', (buf) => {
+          rawData.push(buf);  // buffer to string
+        });
+
+        res.on('end', () => {
+          try {
+            // const obj = JSON.parse(rawData); // string to JSON obj
+            console.log(`------ Data object ${rawData}`);
+            
+          } catch (e) {
+            console.error(`------ Data error : ${e.message}`);
+          }
+          console.log('----- No more data in response.');
+
+        });
+      });
+
+      req.on('error', (e) => {
+        console.error(`----- Problem with request: ${e.message}`);
+      });
+
+      req.write(jsonpayload)
+
+      req.end();
+
+      // Wait for 'rawData' to fed with data
+      await new Promise(resolve => setTimeout(resolve, 5000));
+
+      res.json(rawData)
+
+
+
+      // const url = 'https://api.openai.com/v1/engines/davinci/completions';
+      // const requestData = {
+      //   "prompt": `Say this is a hi`,
+      //   "max_tokens": 7,
+      //   "temperature": 0.5,
+      //   "frequency_penalty": 0.5
+      // };
+      // const headers = {
+      //   'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      // };
+
+      // try {
+      //   const jsonresponse = await got.post(url, { json: requestData, headers: headers }).json();
+      //   output = `${prompt}${jsonresponse.choices[0].text}`;
+      //   console.log(`Success! Here's the output :${output}`);
+      //   return output
+      // } catch (err) {
+      //   console.log(`Agh, erroe caught : ${err}`);
+      // }
   }
 });
 
-app.get('/chat.js', (req, res) => {
-  console.log(__filename)
-  res.sendFile(__filename);
-});
+// app.get('/chat.js', (req, res) => {
+//   res.sendFile(__filename);
+// });
 
-app.get('/logo.png', (req, res) => {
-  res.sendFile(path.join(__dirname, 'logo.png'));
-});
+// app.get('/logo.png', (req, res) => {
+
+//   res.sendFile(path.join(__dirname, 'logo.png'));
+// });
 
 
 
 app.listen(port, () => {
-  console.log('chat server started on port:' + port);
+  console.log('****************** Chat server started on port:' + port + '******************');
 });
